51b6d974707d5a2f474bd849270f20d0cee26386
Glenn Goossens <glgoose@pm.me>
2021-09-09 20:52:34 +0200

    create nextjs app

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home () {
+  return (
+    <div>
+      <Head>
+        <title>Glenn Goossens</title>
+        <meta name='description' content='Webpage Glenn Goossens' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <main></main>
+    </div>
+  )
+}
