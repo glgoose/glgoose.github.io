79c2127a5894f8b823e57e568338d25113676c36
Glenn Goossens <glgoose@pm.me>
2021-09-12 15:44:17 +0200

    add heading & nav

--- pages/projects.js
+++ pages/projects.js
@@ -1,5 +1,28 @@
+import {
+  Center,
+  Container,
+  Heading,
+  Link as ChakraLink,
+  Stack
+} from '@chakra-ui/react'
 import Layout from '../components/Layout'
+import NextLink from 'next/link'
 
 export default function Projects () {
-  return <Layout>Projects</Layout>
+  return (
+    <Layout>
+      <Container maxW='2xl' mt='4vh'>
+        <Stack justify='center'>
+          <Center as='nav'>
+            <NextLink href='/' passHref>
+              <ChakraLink mb={4} me='1em'>
+                &#60; back
+              </ChakraLink>
+            </NextLink>
+          </Center>
+          <Heading size='2xl'>Projects</Heading>
+        </Stack>
+      </Container>
+    </Layout>
+  )
 }
