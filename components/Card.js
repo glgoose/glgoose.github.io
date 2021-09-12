82599d05df4097320632669c8afa6b6f94f18129
Glenn Goossens <glgoose@pm.me>
2021-09-12 23:46:46 +0200

    add card media display

--- components/Card.js
+++ components/Card.js
@@ -1,22 +1,55 @@
-import { useColorModeValue } from '@chakra-ui/color-mode'
-import { Box, Text } from '@chakra-ui/layout'
-import { HStack } from '@chakra-ui/react'
+import {
+  Box,
+  HStack,
+  Image,
+  keyframes,
+  Text,
+  useColorModeValue
+} from '@chakra-ui/react'
+
+const getMediaElement = media => {
+  if (media === null) return
+
+  console.log(media)
+
+  if (media[0].type === 'img') {
+    const slideDown = keyframes`
+			from {transform: translateY(0%)}
+			to {transform: translateY(-90%)}
+		`
+    return (
+      <Image
+        src={media[0].url}
+        alt={media[0].name}
+        fit='cover'
+        animation={
+          media[0].addAnimation
+            ? `${slideDown} 30s linear 2s infinite normal none running`
+            : undefined
+        }
+      />
+    )
+  }
+}
 
 export default function Card ({ project }) {
   return (
-    <Box>
-      <Box
-        bg={useColorModeValue('gray.200', 'gray.700')}
-        w='xs'
-        px={3}
-        py={4}
-        rounded='lg'
-        shadow='lg'
-      >
+    <Box
+      w='xs'
+      h='xs'
+      bg={useColorModeValue('gray.300', 'gray.700')}
+      rounded='lg'
+    >
+      <Box w='100%' h={48} roundedTop='lg' overflow='hidden'>
+        {getMediaElement(project.media)}
+      </Box>
+
+      <Box px={3} py={4}>
         <HStack spacing={3} mb={2}>
           {project.tech.map(technology => (
             <Box
               as='span'
+              key={technology.name}
               fontSize='xs'
               textTransform='uppercase'
               color={useColorModeValue('blue.600', 'blue.300')}
