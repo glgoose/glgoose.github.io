3381cbffcc54edc24e0e8c00850a1f8c13ebb312
Glenn Goossens <glgoose@pm.me>
2021-09-13 21:57:57 +0200

    extract MediaElement

--- components/Card.js
+++ components/Card.js
@@ -14,8 +14,13 @@ import {
 import { useEffect, useRef, useState } from 'react'
 import { FiGithub } from 'react-icons/fi'
 
-const getMediaElement = (media, imgBoxHeight) => {
+const MediaElement = ({ media }) => {
   if (media === null) return
+
+  const [imgBoxHeight, setImgBoxHeight] = useState(null)
+  const imgBoxEl = useRef(null)
+
+  useEffect(() => setImgBoxHeight(imgBoxEl.current.offsetHeight), [imgBoxEl])
 
   const slideDown = keyframes`
 			from {transform: translateY(0px)}
@@ -24,31 +29,6 @@ const getMediaElement = (media, imgBoxHeight) => {
 
   if (media[0].type === 'img') {
     return (
-      <Image
-        src={media[0].url}
-        alt={media[0].name}
-        w='100%'
-        minHeight='100%'
-        fit='cover'
-        align='center top'
-        _hover={
-          media[0].addAnimation && {
-            animation: slideDown + ' 10s linear 0s normal none running'
-          }
-        }
-      />
-    )
-  }
-}
-
-export default function Card ({ project }) {
-  const [imgBoxHeight, setImgBoxHeight] = useState(null)
-  const imgBoxEl = useRef(null)
-
-  useEffect(() => setImgBoxHeight(imgBoxEl.current.offsetHeight), [imgBoxEl])
-
-  return (
-    <Box w='xs' bg={useColorModeValue('gray.300', 'gray.600')} rounded='lg'>
       <Box
         h={52}
         ref={imgBoxEl}
@@ -56,8 +36,28 @@ export default function Card ({ project }) {
         overflow='hidden'
         _hover={{ overflowY: 'scroll' }}
       >
-        {getMediaElement(project.media, imgBoxHeight)}
+        <Image
+          src={media[0].url}
+          alt={media[0].name}
+          w='100%'
+          minHeight='100%'
+          fit='cover'
+          align='center top'
+          _hover={
+            media[0].addAnimation && {
+              animation: slideDown + ' 10s linear 0s normal none running'
+            }
+          }
+        />
       </Box>
+    )
+  }
+}
+
+export default function Card ({ project }) {
+  return (
+    <Box w='xs' bg={useColorModeValue('gray.300', 'gray.600')} rounded='lg'>
+      <MediaElement media={project.media} />
 
       <Flex direction='column' h={44} p={3}>
         <HStack spacing={3} mt={1}>
